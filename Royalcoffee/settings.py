1f3e92e1e3e47dc6009cb2bc65e356e256e44930
Brian Nganga <briannganga70@gmail.com>
2022-10-30 00:04:05 +0300

    secure secrets

--- Royalcoffee/settings.py
+++ Royalcoffee/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# the load_dotenv function gets the environment variables defined in .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$wfb4@$%qrdjox&f@_70&s++57si6uy3vakuq5**^m&3l3ng)'
+#SECRET_KEY = 'django-insecure-)$wfb4@$%qrdjox&f@_70&s++57si6uy3vakuq5**^m&3l3ng)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

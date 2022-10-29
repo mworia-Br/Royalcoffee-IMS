cc683dd5e247c75ac379c6cf1f2119c119b6615e
Brian Nganga <briannganga70@gmail.com>
2022-10-29 23:19:13 +0300

    models

--- Factories/models.py
+++ Factories/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Growerscoop(models.Model):
+    cooperative=models.CharField(max_length=256)
+    county=models.CharField(max_length=256)
+    location=models.CharField(max_length=256)
+    address=models.CharField(max_length=256)
+    total_factories=models.IntegerField(10)
+
+    def __str__(self):
+        return self.cooperative
+
+class Factory(models.Model):
+    factory_name=models.CharField(max_length=256)
+    cooperative=models.CharField(max_length=256)
+    region=models.CharField(max_length=256)
+    variety=models.CharField(max_length=256)
+    altitude=models.CharField(max_length=256)
+    soil=models.CharField(max_length=256)
+    process=models.CharField(max_length=256)
+    total_members=models.IntegerField(max_length=10)
